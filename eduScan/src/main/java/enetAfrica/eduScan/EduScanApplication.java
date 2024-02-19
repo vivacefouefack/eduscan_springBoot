64d6d88a260f06f8e610a03cb5ca07c681072801
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2024-02-19 12:32:38 +0100

    création de l'application et mise en place des dossiers représentant différentes couches

---
+++ eduScan/src/main/java/enetAfrica/eduScan/EduScanApplication.java
@@ -0,0 +1,13 @@
+package enetAfrica.eduScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduScanApplication.class, args);
+	}
+
+}
---
+++ eduScan/src/test/java/enetAfrica/eduScan/EduScanApplicationTests.java
@@ -0,0 +1,13 @@
+package enetAfrica.eduScan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
