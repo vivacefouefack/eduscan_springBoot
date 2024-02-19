15a94741155be2c9ff0740ce6f4b078c4983979a
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2024-02-19 13:15:53 +0100

    création de la classe Role

---
+++ eduScan/src/main/java/enetAfrica/eduScan/model/Role.java
@@ -0,0 +1,20 @@
+package enetAfrica.eduScan.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String description;
+}
